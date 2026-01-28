392f85a13c2b44edc6b115b21745b11655f178fd
dhruv <dhruvkhurana162@gmail.com>
2026-01-28 23:35:08 +0530

    Writting Unit Test for Repo

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -1,12 +1,9 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.request.OrderItemRequest;
 import com.example.demo.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/order")
---
+++ src/test/java/com/example/demo/repository/CustomerRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase
+public class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+}
